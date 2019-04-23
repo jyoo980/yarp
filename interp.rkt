a2b46e09ff01946b30b4969d6e45d53eb416c85c
James Yoo <jyoo980@gmail.com>
2019-04-23 14:35:53 -0700

    Added more cases for interp.rkt

--- interp.rkt
+++ interp.rkt
@@ -8,9 +8,16 @@
   (type-case Expr expr
     [num (n) n]
     [bool (b) b]
+    [id (name) name]
     [add (lhs rhs) (+ (interp lhs) (interp rhs))]
-    [mult (lhs rhs) (* (interp lhs) (interp rhs))]))
+    [mult (lhs rhs) (* (interp lhs) (interp rhs))]
+    [sub (lhs rhs) (- (interp lhs) (interp rhs))]))
 
 (test (interp (num 1)) 1)
 (test (interp (add (num 1) (num 2))) (+ 1 2))
 (test (interp (mult (add (num 1) (num 2)) (num 44))) (* (+ 1 2) 44))
+(test (interp (bool #t)) #t)
+(test (interp (bool #f)) #f)
+(test (interp (id 'fun)) 'fun)
+(test (interp (sub (num 1) (num 3))) -2)
+
