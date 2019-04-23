7e6986e612a90095941776f56b823525c47c8685
James Yoo <jyoo980@gmail.com>
2019-04-23 14:30:39 -0700

    Added more parse cases

--- parser.rkt
+++ parser.rkt
@@ -4,24 +4,44 @@
 
 ;; Expr ::= <num>
 ;;       |  <bool>
+;;       |  <id>
 ;;       |  (+ <Expr> <Expr>)
 ;;       |  (- <Expr> <Expr>)
 ;;       |  (* <Expr> <Expr>)
 
+(define RESERVED-KEYWORDS '(+ - * define let))
+
 (define-type Expr
   [num (? number?)]
   [bool (? boolean?)]
+  [id (? valid-id?)]
   [add (lhs Expr?) (rhs Expr?)]
-  [mult (lhs Expr?) (rhs Expr?)])
+  [mult (lhs Expr?) (rhs Expr?)]
+  [sub (lhs Expr?) (rhs Expr?)])
+
+;; valid-id? : Symbol -> boolean
+;; produce true iff the given symbol is a valid id
+(define (valid-id? s)
+  (and (symbol? s)
+       (not (member s RESERVED-KEYWORDS))))
+
+(test (valid-id? '+) false)
+(test (valid-id? '-) false)
+(test (valid-id? '*) false)
+(test (valid-id? 'define) false)
+(test (valid-id? 'let) false)
+(test (valid-id? 'foo) true)
 
 ;; parse-binop : Symbol Expr Expr -> Expr
 ;; parses a grammar of form (OP <Expr> <Expr>) into its correct representation
 (define (parse-binop op lhs rhs)
   (cond [(symbol=? op '+) (add lhs rhs)]
-        [(symbol=? op '*) (mult lhs rhs)]))
+        [(symbol=? op '*) (mult lhs rhs)]
+        [(symbol=? op '-) (sub lhs rhs)]))
 
 (test (parse-binop '+ (num 1) (num 2)) (add (num 1) (num 2)))
 (test (parse-binop '* (num 1) (num 2)) (mult (num 1) (num 2)))
+(test (parse-binop '- (num 1) (num 2)) (sub (num 1) (num 2)))
 
 ;; parse : String -> Expr
 ;; parses a sequence of strings to an Expr
@@ -29,10 +49,13 @@
   (match sexp
     [(? number?) (num sexp)]
     [(? boolean?) (bool sexp)]
-    [`(,op ,lhs ,rhs) (parse-binop op (parse lhs) (parse rhs))]))
+    [(? valid-id?) (id sexp)]
+    [(list (? symbol? op) lhs rhs) (parse-binop op (parse lhs) (parse rhs))]
+    [_ (error "Unable to parse string: " sexp)]))
 
 (test (parse 1) (num 1))
 (test (parse '(+ 1 2)) (add (num 1) (num 2)))
+(test (parse '(- 1 2)) (sub (num 1) (num 2)))
 (test (parse '(* (+ 1 2) 4)) (mult (add (num 1) (num 2)) (num 4)))
 (test (parse #t) (bool #t))
 (test (parse #f) (bool #f))
