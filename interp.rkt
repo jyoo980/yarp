81b47cbc7fcd91ed77ac9c2f8714e95b6f221b19
James Yoo <jyoo980@gmail.com>
2019-04-25 11:35:25 -0700

    Began to implement substitution scheme

--- interp.rkt
+++ interp.rkt
@@ -2,6 +2,35 @@
 
 (require "parser.rkt")
 
+;; subst : symbol Expr Expr -> Expr
+;; substitution scheme for identifiers, replace all instances of the first arg (symbol)
+;; which are present in the third arg (the body) with the second arg (value)
+(define (subst target-id target-val bound-body)
+  (type-case Expr bound-body
+    [num (n) bound-body]
+    [bool (b) bound-body]
+    [id (name) (if (symbol=? name target-id)
+                   target-val
+                   bound-body)]
+    [add (lhs rhs) (add (subst target-id target-val lhs)
+                        (subst target-id target-val rhs))]
+    [sub (lhs rhs) (sub (subst target-id target-val lhs)
+                        (subst target-id target-val rhs))]
+    [mult (lhs rhs) (mult (subst target-id target-val lhs)
+                          (subst target-id target-val rhs))]
+    [let-expr (bound-id named-expr body-expr)
+              (if (symbol=? bound-id target-id)
+                  (let-expr bound-id
+                            (subst named-expr target-id target-val)
+                            body-expr)
+                  (let-expr bound-id
+                            (subst named-expr target-id target-val)
+                            (subst body-expr target-id target-val)))]))
+
+(test (subst 'x (num 1) (add (num 1) (num 4))) (add (num 1) (num 4)))
+(test (subst 'y (num 4) (add (id 'x) (id 'y))) (add (id 'x) (num 4)))
+(test (subst 'x  (num 4) (add (id 'x) (num 4))) (add (num 4) (num 4)))
+
 ;; interp : String -> <TODO>
 ;; interpreter for the Expr grammar
 (define (interp expr)
@@ -11,7 +40,8 @@
     [id (name) name]
     [add (lhs rhs) (+ (interp lhs) (interp rhs))]
     [mult (lhs rhs) (* (interp lhs) (interp rhs))]
-    [sub (lhs rhs) (- (interp lhs) (interp rhs))]))
+    [sub (lhs rhs) (- (interp lhs) (interp rhs))]
+    [let-expr (name val body) 'TODO]))
 
 (test (interp (num 1)) 1)
 (test (interp (add (num 1) (num 2))) (+ 1 2))
